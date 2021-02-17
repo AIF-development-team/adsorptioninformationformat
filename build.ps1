126bfa0cc0bffccda12396aa7054a8ed37c99f7c
Ren Koh <rkoh@seek.com.au>
2021-02-17 23:18:30 +1100

    install vcpp redist

--- build.ps1
+++ build.ps1
@@ -1,3 +1,9 @@
+Install-Module -Name VcRedist
+Import-Module -Name VcRedist
+$VcList = Get-VcList -Release 2019 -Architecture x64
+Save-VcRedist -Path C:\Temp\VcRedist -VcList $VcList
+Install-VcRedist -Path C:\Temp\VcRedist -VcList $VcList
+
 pip install -r requirements.txt
 pip install pyinstaller
 pyinstaller raw2aif_gui.spec
