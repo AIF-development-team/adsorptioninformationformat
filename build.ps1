8e327cbd4ddcea752937da3848726fb2a58cad21
Ren Koh <rkoh@seek.com.au>
2021-02-17 23:41:22 +1100

    create directory to download vcredist

--- build.ps1
+++ build.ps1
@@ -1,9 +1,10 @@
 Install-PackageProvider -Name NuGet -Force
 Install-Module -Name VcRedist -Force
 Import-Module -Name VcRedist
+New-Item -Path C:\VcRedist -ItemType directory
 $VcList = Get-VcList -Release 2019 -Architecture x64
-Save-VcRedist -Path C:\Temp\VcRedist -VcList $VcList
-Install-VcRedist -Path C:\Temp\VcRedist -VcList $VcList
+Save-VcRedist -Path C:\VcRedist -VcList $VcList
+Install-VcRedist -Path C:\VcRedist -VcList $VcList
 
 pip install -r requirements.txt
 pip install pyinstaller
